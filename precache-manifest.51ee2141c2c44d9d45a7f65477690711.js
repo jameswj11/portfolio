44345ce41fc759c63c64918800fb42b82aedb2af
James Johnson <jamesjohnson@Jamess-MBP.home>
2020-04-07 23:33:40 -0400

    Updates

---
+++ precache-manifest.51ee2141c2c44d9d45a7f65477690711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c691e1f95c7e0a69fb4de76568bd9d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ab2d1f94f40f89f11e4",
+    "url": "/static/css/main.c48579c8.chunk.css"
+  },
+  {
+    "revision": "fdf3576cc194dbbe402c",
+    "url": "/static/js/2.64b26760.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.64b26760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab2d1f94f40f89f11e4",
+    "url": "/static/js/main.788e3ca4.chunk.js"
+  },
+  {
+    "revision": "84959b8278325eadbbae",
+    "url": "/static/js/runtime-main.184c63b7.js"
+  }
+]);
